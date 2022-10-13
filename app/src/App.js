c0e27d8d145ccefc5dd4bc7666e3ef7f8b2cb90e
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-13 12:52:11 +0000

    kept working in main accidently. moving to new branch

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react';
 import React from "react";
+import Cards from './Cards';
 
 export default function App() {
     // // const [ page, setPage] = useState('home')
     const [data, setData] = useState([])
-    const [page, setPage] = useState
+    // const [page, setPage] = useState
     useEffect(() => {
         async function getPost() {
             const response = await axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/')
@@ -15,48 +16,56 @@ export default function App() {
         
     }, [])
     console.log(data)
+    
+    // const dFoodFilter = data.filter(dinner =>
+    //     dinner.title === 'Dinner'
+    //   );
+    // const dinnerList = dFoodFilter.map(dinnerMenu => 
+    //     <li>
+    //         <div>
+    //             title={dinnerMenu.title}
+    //         </div>
+    //         <div>
 
-    if (post.length != 0) {
-    return (
+    //         </div>
+    //     </li>
+    //     )
     
-        <h1 className = 'text-center'>{data[0].title}</h1>
-    )
-    }   
-    
-
-    // <h1>{post[0].title}</h1>
-        
-    
-    //     // <>  
-    //     //     {page == 'home' && <Home />}
-    //     //     {page == 'lunch' && <h1>LUNCH</h1>}
-    //     //     {page == 'dinner' && <h1>DINNER</h1>}
-    //     // </> 
+    if (data.length != 0) {
+        return (
+            <Cards
+            title = {data[0].title}
+            price = {data[0].price}
+            description = {data[0].description}
+            />
+            
+         
+        )
+  
     
 }
+}
 
-// function grabData() {
-//     const [post, setPost] = React.useState([])
-
-//     React.useEffect(() => {
-//         async function getPost() {
-//             const response = await axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/')
-//             // console.log(response)
-//             setPost(response.data)
-//         }
-//         getPost();
-//         // console.log(post)
-//     }, [])
-// }
-// function Home() {
-//     return (
-//     <h1>{post[0]}</h1>
+// function Cards({
+//     title,
+//     price,
+//     description
+//     }) {
+//     return(
+//     <>
+//     <div className = 'container'>
+//                 <div className = 'row'>
+//                     <div className = 'col-lg-6 col-md-12'>
+//                         <div className="card"> 
+//                             <div className="card-body">
+//                                 <h3 className="card-title text-center ">{title}</h3>
+//                                 <h5 className="card-text text-center">{price}</h5>
+//                                 <div className="card-text text-center">{description}</div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//     </> 
 //     )
-// }
-// async function CallData () {
-//         let response = await axios.get(`https://astute-baton-362318.ue.r.appspot.com/api/json/`)
-//         console.log(response)
-//     // return (
-        
-//     // )
 // }
