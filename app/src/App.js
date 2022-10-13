aa71f0f4893b359e6e91d3f69aecaaff64d6114f
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-13 14:19:45 +0000

    have a list to dynamically generate all menu items with hard coded filter. next step is to get page state and buttons to switch what is being filtered/what data is being displayed by the API

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react';
 import React from "react";
-import Cards from './Cards';
+// import Cards from './Cards';
 
 export default function App() {
-    // // const [ page, setPage] = useState('home')
+    // const [ page, setPage] = useState('home')
     const [data, setData] = useState([])
-    // const [page, setPage] = useState
     useEffect(() => {
         async function getPost() {
             const response = await axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/')
@@ -17,33 +16,48 @@ export default function App() {
     }, [])
     console.log(data)
     
-    // const dFoodFilter = data.filter(dinner =>
-    //     dinner.title === 'Dinner'
-    //   );
-    // const dinnerList = dFoodFilter.map(dinnerMenu => 
-    //     <li>
-    //         <div>
-    //             title={dinnerMenu.title}
-    //         </div>
-    //         <div>
+    let dFoodFilter = data.filter(dinner =>
+        dinner.category.title === 'Dinner'
+      );
+      console.log(dFoodFilter)
+    let dinnerItems = []
+    for (let i=0; i<dFoodFilter.length; i++){
+        dinnerItems.push(
+            
+<>
+    <div className = 'container'>
+                <div className = 'row'>
+                    <div className = 'col-lg-4 col-md-12'>
+                        <div className="card"> 
+                            <div className="card-body">
+                                <h3 className="card-title text-center ">{dFoodFilter[i].title}</h3>
+                                <h5 className="card-text text-center">{dFoodFilter[i].price}</h5>
+                                <div className="card-text text-center">{dFoodFilter[i].description}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </> 
 
-    //         </div>
-    //     </li>
-    //     )
-    
-    if (data.length != 0) {
-        return (
-            <Cards
-            title = {data[0].title}
-            price = {data[0].price}
-            description = {data[0].description}
-            />
+            
+        )
+       
+    }
+    return <>{dinnerItems}</>    
+//     if (data.length != 0) {
+//         return (
+//             <Cards
+//             title = {data[0].title}
+//             price = {data[0].price}
+//             description = {data[0].description}
+//             />
             
          
-        )
+//         )
   
     
-}
+// }
 }
 
 // function Cards({
