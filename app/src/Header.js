b919e6a81f2a8474a8407367fc125ebf14ccaa0b
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-17 13:08:12 +0000

    google font, done

--- app/src/Header.js
+++ app/src/Header.js
@@ -6,9 +6,9 @@ export default function Header(props) {
         className="row text-center align-items-center hover text-white text-opacity-75 bg-dark rounded fst-italic"
         onClick={() => props.handleClick(props.text)}
       >
-        <div className="col-4">la meilleure nourriture avec les meilleurs ingrédients</div>
-        <h1 className="col-4">Faire Réagir La Nourriture</h1>
-        <h4 className="col-4">Open from 9-2 and 5-10 daily</h4>
+        <h5 className="col-4 cursive">la meilleure nourriture avec les meilleurs ingrédients</h5>
+        <h1 className="col-4 cursive">Faire Réagir La Nourriture</h1>
+        <h4 className="col-4 cursive">Open from 9-2 and 5-10 daily</h4>
       </div>
     </div>
   );
